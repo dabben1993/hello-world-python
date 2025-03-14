2345656b1fea5328d23094866e0c58c93d9d392b
dabben1993 <gickis1993@live.com>
2025-03-14 11:08:39 +0100

    refactor tests

--- Math/tests/test_math_functions.py
+++ Math/tests/test_math_functions.py
@@ -1,4 +1,8 @@
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from math_functions import add, subtract, multiply, divide
 
 class TestMathFunctions(unittest.TestCase):
